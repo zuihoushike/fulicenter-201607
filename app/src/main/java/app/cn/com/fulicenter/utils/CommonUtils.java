73051f9965c81258a186cade648470b34964d87d
kiril <m15229705442@163.com>
2016-10-13 15:32:18 +0800

    second project

---
+++ app/src/main/java/app/cn/com/fulicenter/utils/CommonUtils.java
@@ -0,0 +1,20 @@
+package app.cn.com.fulicenter.utils;
+
+import android.widget.Toast;
+
+import cn.ucai.fulicenter.FuLiCenterApplication;
+
+public class CommonUtils {
+    public static void showLongToast(String msg){
+        Toast.makeText(FuLiCenterApplication.getInstance(),msg,Toast.LENGTH_LONG).show();
+    }
+    public static void showShortToast(String msg){
+        Toast.makeText(FuLiCenterApplication.getInstance(),msg,Toast.LENGTH_SHORT).show();
+    }
+    public static void showLongToast(int rId){
+        showLongToast(FuLiCenterApplication.getInstance().getString(rId));
+    }
+    public static void showShortToast(int rId){
+        showShortToast(FuLiCenterApplication.getInstance().getString(rId));
+    }
+}
