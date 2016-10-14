9b1cb2d02eefd6781fc800b6b4f8985642ec60dc
kiril <m15229705442@163.com>
2016-10-14 16:49:55 +0800

    完成闪屏

---
+++ app/src/main/java/app/cn/com/fulicenter/SplashActivity.java
@@ -0,0 +1,38 @@
+package app.cn.com.fulicenter;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+
+import app.cn.com.fulicenter.utils.MFGT;
+
+
+public class SplashActivity extends AppCompatActivity {
+    static final long SPLAtTH_Time = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long startTime = SystemClock.currentThreadTimeMillis();
+                long castTime = SystemClock.currentThreadTimeMillis()-startTime;
+                if (SPLAtTH_Time-castTime>0){
+                    try {
+                        Thread.sleep(SPLAtTH_Time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                MFGT.gotoMainActivity(SplashActivity.this);
+                finish();
+            }
+        }).start();
+    }
+}
---
+++ app/src/main/java/app/cn/com/fulicenter/utils/MFGT.java
@@ -0,0 +1,26 @@
+package app.cn.com.fulicenter.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import app.cn.com.fulicenter.MainActivity;
+import app.cn.com.fulicenter.R;
+
+/**
+ * Created by 最后时刻 on 2016/10/14.
+ */
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void gotoMainActivity(Activity context){
+        startActivity(context, MainActivity.class);
+    }
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_right_out);
+    }
+}
