f2c46a0b33f7d7b8cefcbd79a0bc23e13d3f5e94
kiril <m15229705442@163.com>
2016-10-19 16:03:57 +0800

    精选首页的adapter编写

---
+++ app/src/main/java/app/cn/com/fulicenter/Adapter/BoutiqueAdapter.java
@@ -0,0 +1,101 @@
+package app.cn.com.fulicenter.Adapter;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import app.cn.com.fulicenter.I;
+import app.cn.com.fulicenter.R;
+import app.cn.com.fulicenter.bean.BoutiqueBean;
+import app.cn.com.fulicenter.utils.ImageLoader;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BoutiqueAdapter extends Adapter {
+    Context mContext;
+    ArrayList<BoutiqueBean> mList;
+    boolean isMore;
+
+    public BoutiqueAdapter(ArrayList<BoutiqueBean> mList, Context mContext) {
+        mList = new ArrayList<>();
+        this.mList = mList;
+        this.mContext = mContext;
+    }
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ViewHolder holder = null;
+        if (viewType == I.TYPE_FOOTER) {
+            holder = new GoodsAdapter.FooterViewHolder(LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_footer, parent, false));
+        } else {
+            holder = new BoutqueViewHolder(LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_boutique, parent, false));
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (holder instanceof GoodsAdapter.FooterViewHolder){
+            ((GoodsAdapter.FooterViewHolder) holder).tvFooter.setText(getFooterString());
+        }
+        if (holder instanceof BoutqueViewHolder){
+            BoutiqueBean boutiqueBean = mList.get(position);
+            ImageLoader.downloadImg(mContext,((BoutqueViewHolder) holder).ivBoutiqueImg,boutiqueBean.getImageurl());
+            ((BoutqueViewHolder) holder).ivBoutiqueTitle.setText(boutiqueBean.getTitle());
+            ((BoutqueViewHolder) holder).tvBoutiqueName.setText(boutiqueBean.getName());
+            ((BoutqueViewHolder) holder).tvBoutiqueDescription.setText(boutiqueBean.getDescription());
+        }
+    }
+
+    private int getFooterString() {
+        return isMore?R.string.load_more:R.string.no_more;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size() + 1 : 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        }
+        return I.TYPE_ITEM;
+    }
+
+    static class BoutqueViewHolder  extends RecyclerView.ViewHolder{
+        @BindView(R.id.ivBoutiqueImg)
+        ImageView ivBoutiqueImg;
+        @BindView(R.id.ivBoutiqueTitle)
+        TextView ivBoutiqueTitle;
+        @BindView(R.id.tvBoutiqueName)
+        TextView tvBoutiqueName;
+        @BindView(R.id.tvBoutiqueDescription)
+        TextView tvBoutiqueDescription;
+
+        BoutqueViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
