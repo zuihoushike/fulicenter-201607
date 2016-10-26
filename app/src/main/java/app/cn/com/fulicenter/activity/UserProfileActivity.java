12fbe67df0ebc9a85bb7bd2e3d95737e58b2a9b5
kiril <m15229705442@163.com>
2016-10-26 09:14:42 +0800

    更新用户头像

--- app/src/main/java/app/cn/com/fulicenter/activity/UserProfileActivity.java
+++ app/src/main/java/app/cn/com/fulicenter/activity/UserProfileActivity.java
@@ -1,7 +1,6 @@
 package app.cn.com.fulicenter.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
--- app/src/main/java/app/cn/com/fulicenter/fragment/PersonalCenterFragment.java
+++ app/src/main/java/app/cn/com/fulicenter/fragment/PersonalCenterFragment.java
@@ -7,7 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import app.cn.com.fulicenter.FuLiCenterApplication;
 import app.cn.com.fulicenter.R;
@@ -48,16 +52,18 @@ public class PersonalCenterFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        initOrderList();
     }
+
 
     @Override
     protected void initData() {
         User user = FuLiCenterApplication.getUser();
-        L.e(TAG,"user="+user);
-        if (user==null){
+        L.e(TAG, "user=" + user);
+        if (user == null) {
             MFGT.gotoLogin(mContext);
-        }else {
-            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,mIvUserAvatar);
+        } else {
+            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user), mContext, mIvUserAvatar);
             mTvUserName.setText(user.getMuserNick());
         }
     }
@@ -67,15 +73,33 @@ public class PersonalCenterFragment extends BaseFragment {
 
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        initData();
-    }
+
 
     @OnClick({R.id.tv_center_settings,R.id.center_user_info})
     public void gotoSettings(){
         MFGT.gotoSettings(mContext);
+    }
+
+    private void initOrderList() {
+        ArrayList<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> order1 = new HashMap<String, Object>();
+        order1.put("order", R.drawable.order_list1);
+        data.add(order1);
+        HashMap<String, Object> order2 = new HashMap<String, Object>();
+        order2.put("order", R.drawable.order_list2);
+        data.add(order2);
+        HashMap<String, Object> order3 = new HashMap<String, Object>();
+        order3.put("order", R.drawable.order_list3);
+        data.add(order3);
+        HashMap<String, Object> order4 = new HashMap<String, Object>();
+        order4.put("order", R.drawable.order_list4);
+        data.add(order4);
+        HashMap<String, Object> order5 = new HashMap<String, Object>();
+        order5.put("order", R.drawable.order_list5);
+        data.add(order5);
+        SimpleAdapter adapter = new SimpleAdapter(mContext, data, R.layout.simple_adapter,
+                new String[]{"order"}, new int[]{R.id.iv_order});
+        mCenterUserOrderLis.setAdapter(adapter);
     }
 
 
