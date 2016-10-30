97aeb1b8ae68b731d7577cec2a146a30c6d38b8c
kiril <m15229705442@163.com>
2016-10-30 21:42:34 +0800

    订单页面的支付数据准备

--- app/src/main/java/app/cn/com/fulicenter/activity/OrderActivity.java
+++ app/src/main/java/app/cn/com/fulicenter/activity/OrderActivity.java
@@ -9,7 +9,18 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import app.cn.com.fulicenter.FuLiCenterApplication;
+import app.cn.com.fulicenter.I;
 import app.cn.com.fulicenter.R;
+import app.cn.com.fulicenter.bean.CartBean;
+import app.cn.com.fulicenter.bean.User;
+import app.cn.com.fulicenter.net.NetDAO;
+import app.cn.com.fulicenter.net.OkHttpUtils;
+import app.cn.com.fulicenter.utils.L;
+import app.cn.com.fulicenter.utils.ResultUtils;
+import app.cn.com.fulicenter.view.DisplayUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -28,22 +39,77 @@ public class OrderActivity extends BaseActivity {
     @BindView(R.id.tv_order_price)
     TextView mTvOrderPrice;
 
+    OrderActivity mContext;
+    User user = null;
+    String cartIds = "";
+    ArrayList<CartBean> mList = null;
+    String[] ids = new String[]{};
+    int rankPrice = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_order);
         ButterKnife.bind(this);
+        mContext = this;
+        mList = new ArrayList<>();
         super.onCreate(savedInstanceState);
     }
 
     @Override
     protected void initView() {
-
+        DisplayUtils.initBackWithTitle(mContext,getString(R.string.confirm_order));
     }
 
     @Override
     protected void initData() {
-        String cartIds = getIntent().getStringExtra(I.Cart.ID);
+        cartIds = getIntent().getStringExtra(I.Cart.ID);
+        user = FuLiCenterApplication.getUser();
         L.e(TAG,"cartIds="+cartIds);
+        if(cartIds==null || cartIds.equals("")
+                || user==null){
+            finish();
+        }
+        ids = cartIds.split(",");
+        geOrderList();
+    }
+
+    private void geOrderList() {
+        NetDAO.downloadCart(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                ArrayList<CartBean> list = ResultUtils.getCartFromJson(s);
+                if(list==null || list.size()==0){
+                    finish();
+                }else{
+                    mList.addAll(list);
+                    sumPrice();
+                }
+            }
+            @Override
+            public void onError(String error) {
+            }
+        });
+    }
+
+    private void sumPrice() {
+        rankPrice = 0;
+        if(mList!=null && mList.size()>0){
+            for (CartBean c:mList){
+                L.e(TAG,"c.id="+c.getId());
+                for (String id:ids) {
+                    L.e(TAG,"order.id="+id);
+                    if (id.equals(String.valueOf(c.getId()))) {
+                        rankPrice += getPrice(c.getGoods().getRankPrice()) * c.getCount();
+                    }
+                }
+            }
+        }
+        mTvOrderPrice.setText("合计:￥"+Double.valueOf(rankPrice));
+    }
+
+    private int getPrice(String price){
+        price = price.substring(price.indexOf("￥")+1);
+        return Integer.valueOf(price);
     }
 
     @Override
